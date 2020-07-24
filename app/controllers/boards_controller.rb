d3dc1950e48eaac5b836c7823d9fce1802022dd1
hank <hank8451@gmail.com>
2020-07-24 18:02:00 +0800

    程式碼整理

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,19 +1,19 @@
 class BoardsController < ApplicationController
 
+  before_action :find_board, only: [:show, :edit, :update, :destroy]
+  # before_action :find_board, except: [:index, :new, :create, ...]
+
   def index
-    @boards = Board.all
+    @boards = Board.where(deleted_at: nil)
   end
 
   def show
-    @board = Board.find(params[:id])
   end
 
   def new
     @board = Board.new
   end
-
   
-
   def create 
     @board = Board.new(board_params) 
     if @board.save
@@ -26,12 +26,9 @@ class BoardsController < ApplicationController
   end
 
   def edit
-    @board = Board.find(params[:id])
   end
 
   def update
-    @board = Board.find(params[:id])
-    
     if @board.update(board_params)
       redirect_to boards_path, notice: "更新成功"
     else
@@ -39,9 +36,15 @@ class BoardsController < ApplicationController
     end
   end
 
- 
+  def destroy
+    @board.destroy
+    redirect_to boards_path, notice: "刪除成功"
+  end
 
   private
+  def find_board
+    @board = Board.find(params[:id])
+  end
   def board_params
     params.require(:board).permit(:title, :intro)
   end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,7 @@
 class Board < ApplicationRecord
   validates :title, presence: true, length: {minimum: 2}
+  
+  def destroy
+    update(deleted_at: Time.now)
+  end
 end
