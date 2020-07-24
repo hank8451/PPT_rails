b3c4ffd4254ef65a060a08469b6e9a078e867539
hank <hank8451@gmail.com>
2020-07-24 16:35:31 +0800

    CRUD

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,25 +1,45 @@
 class BoardsController < ApplicationController
+
   def index
     @boards = Board.all
+  end
+
+  def show
+    @board = Board.find(params[:id])
   end
 
   def new
     @board = Board.new
   end
 
-  def create
-    
-    @board = Board.new(board_params)
-   
+  
+
+  def create 
+    @board = Board.new(board_params) 
     if @board.save
       # flash[:notice] = "新增成功"
       # redirect_to boards_path 
       redirect_to boards_path, notice: "新增成功"
     else
-
+      render :new
     end
-
   end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    
+    if @board.update(board_params)
+      redirect_to boards_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+ 
 
   private
   def board_params
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,3 @@
 class Board < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: {minimum: 2}
 end
